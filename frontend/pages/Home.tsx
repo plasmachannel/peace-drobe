3cb0599e9bbe7e4ad69a65e0e9ba7ba8e40448db
Thomas Marlette <tplasmachannel@gmail.com>
2024-01-18 22:40:52 -0500

    Used design system components instead of styling

--- frontend/pages/Home.tsx
+++ frontend/pages/Home.tsx
@@ -1,10 +1,12 @@
-import {Button, Menu} from "react-native-paper";
-import {JSX} from "react";
-import {StyleSheet, Text, View} from "react-native";
+import {BottomNavigation, Button, Divider, Menu, SegmentedButtons} from "react-native-paper";
+import {JSX, useState} from "react";
+import {Platform, StyleSheet, Text, View} from "react-native";
 import SearchIcon from "../icons/SearchIcon";
 import RandomLogo from "../icons/RandomLogo";
 import FilterIcon from "../icons/FilterIcon";
 import {getExpressTestInfo} from "../service-clients/firstRequest";
+import { Appbar } from 'react-native-paper';
+import App from "../App";
 
 
 /*
@@ -28,56 +30,83 @@ const navigationStyle = StyleSheet.create({
 
 const NavigationToolBar = (): JSX.Element => {
   const toolbarHeight = 150;
-  return (<View style={navigationStyle.container}>
-      <SearchIcon height={toolbarHeight} width={25}/>
-      <RandomLogo height={toolbarHeight} width={180} />
-      <FilterIcon height={toolbarHeight} width={25} />
-  </View>);
+  const MORE_ICON = Platform.OS === 'ios' ? 'dots-horizontal' : 'dots-vertical';
+
+  return (
+    <Appbar>
+      <Appbar.Content title="Title" subtitle={'Subtitle'} />
+      <Appbar.Action icon="magnify" onPress={() => {}} />
+      <Appbar.Action icon={MORE_ICON} onPress={() => {}} />
+    </Appbar>);
 }
+/*
+ <View style={navigationStyle.container}>
+        <SearchIcon height={toolbarHeight} width={25}/>
+        <RandomLogo height={toolbarHeight} width={180} />
+        <FilterIcon height={toolbarHeight} width={25} />
+      </View>
+ */
 
 
-const inventoryTabStyle = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-  },
-});
 const InventoryTabs = (): JSX.Element => {
-  return (<View
-    style={inventoryTabStyle.container}
-  >
-      <Button
-        mode="text"
-        onPress={() => {console.log(getExpressTestInfo().then(data => console.log(data)))}}
-      >
-        Items (0)
-      </Button>
-      <Button
-        mode="text"
-        onPress={() => {console.log('hello')}}
-      >
-        Outfits (0)
-      </Button>
-      <Button
-        mode="text"
-        onPress={() => {console.log('hello')}}
-      >
-        Collections (0)
-      </Button>
-    </View>);
+  const [selectedTab, setSelectedTab] = useState('items');
+  return (
+    <SegmentedButtons
+      value={selectedTab}
+      onValueChange={setSelectedTab}
+      buttons={[
+        {
+          value: 'items',
+          label: 'Items (0)',
+        },
+        {
+          value: 'outfits',
+          label: 'Outfits (0)',
+        },
+        { value: 'collections', label: 'Collections (0)' },
+      ]}
+    />);
 }
 
 const SomeBody = () : JSX.Element => {
   return <View/>;
 }
 
+const MusicRoute = () => <Text>home</Text>;
 
-const MenuSelect = () : JSX.Element => {
-  return <View></View>;
-}
+const AlbumsRoute = () => <Text>wardrobe</Text>;
+
+const RecentsRoute = () => <Text>Recents</Text>;
+
+const NotificationsRoute = () => <Text>shop</Text>;
+
+// https://pictogrammers.com/library/mdi/
+  // https://callstack.github.io/react-native-paper/docs/guides/icons/
+  // https://oblador.github.io/react-native-vector-icons/
+const BottomMenu = () => {
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: 'home', title: 'Home', focusedIcon: 'home', unfocusedIcon: 'home-outline'},
+    { key: 'wardrobe', title: 'Wardrobe', focusedIcon: 'wardrobe', unfocusedIcon: 'wardrobe-outline' },
+    { key: 'recents', title: 'Recents', focusedIcon: 'history' },
+    { key: 'shop', title: 'Shop', focusedIcon: 'shopping-search', unfocusedIcon: 'shopping-search' },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    home: MusicRoute,
+    wardrobe: AlbumsRoute,
+    recents: RecentsRoute,
+    shop: NotificationsRoute,
+  });
+
+  return (
+    <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
+  );
+};
 
 
 const homepageStyle = StyleSheet.create({
@@ -95,10 +124,11 @@ const HomeScreen = ({navigation}): JSX.Element => {
     <View
       style={homepageStyle.container}
     >
+
       <NavigationToolBar/>
       <InventoryTabs />
       <SomeBody/>
-      <MenuSelect/>
+      <BottomMenu/>
     </View>
   </>);
 };
